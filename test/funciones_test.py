1b994533b0ff6dce5e2aab78096b57d21a372e0f
eantoniocalo18 <e.antoniocalo18@go.ugr.es>
2021-12-14 12:54:47 +0100

    Avanza M1, implementando nuevas funciones para ejecutar simulación de test
    Se ha creado una nueva función almacen_from_CSV() que permitirá cargar una serie de productos junto con sus cantidades
    Esto será útil a la hora de simular test con datos ficticios
    Avanza en M1

--- test/funciones_test.py
+++ test/funciones_test.py
@@ -1,12 +1,33 @@
 import csv
-
+from src.producto import Producto
 # Función que leerá los datos de un fichero CSV y los pasará a un diccionario de productos
 
-def from_CSV(path):
-    with open('data.txt', mode='r') as csv_file:
+def almacen_from_CSV(path):
+    with open(path, mode='r') as csv_file:
+        productos=products_from_CSV(path)
         csv_reader = csv.reader(csv_file) 
-        diccionario= {rows[0]:rows[1] for rows in csv_reader}
+        diccionario = {}
+        i=0
+        for rows in csv_reader:
+            diccionario[productos[i]]= rows[6]
+            i+=1
     return diccionario
+
+
+def products_from_CSV(path):
+    with open(path, mode='r') as csv_file:
+        csv_reader = csv.reader(csv_file) 
+        listado_productos= []
+        for rows in csv_reader:
+            id= rows[0]
+            nombre= rows[1]
+            precio = rows[2]
+            espacio = rows[3]
+            cantidad_baja = rows[4]
+            cantidad_alta = rows[5]
+            producto = Producto(id,nombre,precio,espacio,cantidad_baja, cantidad_alta)
+            listado_productos.append(producto)
+    return listado_productos
     
     
 

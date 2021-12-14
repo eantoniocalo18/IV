31a2fcb106dc7bce2d2076f415b326f45a5ebff3
eantoniocalo18 <e.antoniocalo18@go.ugr.es>
2021-12-14 12:56:28 +0100

    Avanza M1, Closes #10 Testeo del estado de un almacén
    Se ha creado el test estado_almacén que permite comprobar que el estado de un almacén es correcto.
    Realiza las siguientes comprobaciones:
    - Los productos indicados no están vacíos
    - El almacén indicado no está vacío
    - El espacio usado del almacén es >0
    - El espacio del almacén es < que las dimensiones del almacén
    
    Código que no está probado es código roto, en este caso, ya tenemos la base de que cuando accedamos al estado de un almacén va a ser un valor correcto.
    Por lo tanto, este test nos sirve de base para implementar los demás test indicados en M1.
    Por otro lado, se ha incluido un archivo data.txt que suministra la información necesaria para ejecutar el test y ver que realmente funciona.
    
    Avanza en M1 y closes #10

--- test/test_estado_almacen.py
+++ test/test_estado_almacen.py
@@ -1,3 +1,25 @@
 import pytest
 from assertpy import assert_that
+from funciones_test import *
+from src.producto import Producto
+from src.inventario import Inventario
 
+def test1():
+    # Cargamos productos
+    productos = products_from_CSV('test/data.txt')
+    
+    # Cargamos el almacen
+    almacen = almacen_from_CSV('test/data.txt')
+    
+    # Creamos un inventario
+    inventario = Inventario(1,almacen,100)
+    
+    # Comprobamos que ambos NO están vacíos
+    assert_that(productos).is_not_empty()
+    assert_that(almacen).is_not_empty()
+    
+    # Comprobamos que el espacio es >0 y < dimensiones del almacen
+    assert_that(inventario.get_espacio_usado()).is_greater_than(0)
+    #print (inventario.get_dimensiones())
+    assert_that(inventario.get_espacio_usado()).is_less_than(inventario.get_dimensiones())
+    

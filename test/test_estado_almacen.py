97ba0e9c96bb9643b16b39edf69797cb2e23be31
Antonio Caño Lopez <e.antoniocalo18@go.ugr.es>
2021-12-15 18:11:57 +0100

    Añadiendo comprobaciones extra
    Se ha añadido la comprobación de que cada producto cargado es realmente un objeto de la clase Producto.
    Cambios realizados basados en las sugerencias de @Balrrach en #32

--- test/test_estado_almacen.py
+++ test/test_estado_almacen.py
@@ -15,12 +15,15 @@ def test_estado_almacen():
     inventario = Inventario(1,almacen,100)
     
     # Comprobamos que ambos NO están vacíos
-    assert_that(productos, description='Se han cargado mal los productos').is_not_empty()
-    assert_that(almacen, description='Se ha cargado mal el inventario').is_not_empty()
+    for producto in productos:
+        assert_that(producto,description='Se ha cargado mal el producto').is_instance_of(Producto)
+        
+   
+    assert_that(almacen, description='Se ha cargado mal el almacen').is_not_empty()
     
     # Comprobamos que el espacio es >0 y < dimensiones del almacen
     assert_that(inventario.get_espacio_usado()).is_greater_than(0)
-    #print (inventario.get_dimensiones())
+    
     assert_that(inventario.get_espacio_usado(), description='El espacio del almacén tiene valores erróneos').is_less_than(inventario.get_dimensiones())
 
 

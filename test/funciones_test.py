76b601a83b8aaaae3a3000a784212ad7d5121477
Antonio Caño Lopez <e.antoniocalo18@go.ugr.es>
2021-12-21 17:05:24 +0100

    Fixing typo
    Eliminando comentarios inline

--- test/funciones_test.py
+++ test/funciones_test.py
@@ -1,16 +1,13 @@
 import csv
 from src.producto import Producto
-# Función que leerá los datos de un fichero CSV y los pasará a un diccionario de productos
 
 def almacen_from_CSV(path):
     with open(path, mode='r') as csv_file:
         productos=products_from_CSV(path)
         csv_reader = csv.reader(csv_file) 
         diccionario = {}
-        #el diccionario tiene como clave un objeto de tipo Producto y de valor una cantidad
         i=0
         for rows in csv_reader:
-        # Añadimos a la clave 'Producto' la cantidad de productos que tenemos
             diccionario[productos[i]]= rows[6]
             i+=1
     return diccionario
